c97f948b1d1e51be8f32a79122b0286f68875d1a
DM42.Net (Matt Dent) <dentm42@dm42.net>
2018-01-12 23:29:13 -0500

    Add open/closed order queries.

--- cart/myshop.php
+++ cart/myshop.php
@@ -1,22 +1,48 @@
 <?php
 
-
-function cart_load(){
+function cart_myshop_load(){
 	Zotlabs\Extend\Hook::register('cart_main_myshop', 'addon/cart/cart.php', 'cart_construct_page');
-
 }
 
-function cart_unload(){
-	//Zotlabs\Extend\Hook::unregister('cart_post_add_item','addon/cart/cart.php','cart_post_add_item');
-
+function cart_myshop_unload(){
+	Zotlabs\Extend\Hook::unregister('cart_main_myshop', 'addon/cart/cart.php', 'cart_construct_page');
 }
 
-function cart_myshop_openorders () {
+function cart_myshop_openorders ($limit=100,$offset=1) {
 
+  $seller_hash=get_observer_hash();
+  $r=q("select unique(cart_order.order_hash) as ohash from cart_order,cart_orderitems
+        where cart_order.order_hash = cart_orderitems.orderhash and
+        seller_channel = '%s' and cart_orderitems.item_fulfilled=false
+        limit=%i offset=%i",
+      dbesc($seller_hash),
+      intval($limit), intval($offset));
+
+  if (!$r) {return Array();}
+
+  foreach ($r as $order) {
+    $orders[] = cart_loadorder($order["ohash"]);
+  }
+  return $orders;
 }
 
-function cart_myshop_closedorders () {
+function cart_myshop_closedorders ($limit=100,$offset=1) {
 
+  $seller_hash=get_observer_hash();
+  $r=q("select order_hash as ohash from cart_order where
+        seller_channel = '%s' and
+        ohash not in (select order_hash from cart_orderitems
+        where item_fulfilled item_fulfilled=false)
+        limit=%i offset=%i",
+      dbesc($seller_hash),
+      intval($limit), intval($offset));
+
+  if (!$r) {return Array();}
+
+  foreach ($r as $order) {
+    $orders[] = cart_loadorder($order["ohash"]);
+  }
+  return $orders;
 }
 
 function cart_myshop_pagecontent(&$pagecontent) {
@@ -38,5 +64,4 @@ function cart_myshop_pagecontent(&$pagecontent) {
     $pagecontent .= "<h4>Access denied.</h4>";
   }
 
-  
 }
